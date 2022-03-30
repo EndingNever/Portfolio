915f699de75117b908f2c2eed237624912c6a8bd
Fabian Villasenor <exionite@gmail.com>
2022-03-30 09:26:58 -0700

    icons

--- src/Components/AboutMe/About.js
+++ src/Components/AboutMe/About.js
@@ -6,9 +6,9 @@ export default function About() {
     <div className="information-container">
       <div className='about-container'>
         <p>Hello!</p>
-        <p>My name is Fabian Villaseñor, a Front End Web Developer. I began coding when I found a FREE coding academy teaching web development using Angular only five minutes away in Modesto, California.
+        <p>My name is Fabian Villaseñor, a Front End Web Developer. I began coding when I found a FREE coding academy teaching web development using Angular in my hometown of Modesto, California.
           I had never coded before so I was intimidated at first, however once I started learning I was hooked! Since my completion of the bootcamp I have continued my coding journey by learning new things and creating projects, some of which you can find below!
-          Some tools I have used to create these projects are React, Javascript, Angular, MySQL, REST APIs, jsonwebtoken, and Node.js
+          Some tools I have used to create these projects are React, Javascript, Angular, MySQL, REST APIs, and Node.js
         </p>
       </div>
       <div className="about-icons">
