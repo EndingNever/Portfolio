e1d7e85c96b12cd840ab532eb9494a91c6edd501
Fabian Villasenor <exionite@gmail.com>
2022-03-30 16:45:01 -0700

    nav

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,18 +1,24 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 import './Navbar.css'
 import Circle from '../Circle/Circle'
 import { Link } from 'react-router-dom'
 
 export default function Navbar() {
+    
+
+
+
     return (
         <div className='navbar-container'>
+            <a id="navbar"></a>
             <div className="nav-circle">
                 <div className="circle"></div>
                 <div className="name">
                     <h1>Fabian Villaseñor</h1>
                 </div>
                 <div className="navbar-items-container">
-                    <div className="navbar-items"><Link to={'/'}>Home</Link></div>
+                    <div className="navbar-items"><a href='#navbar' >Home</a></div>
                     <div className="navbar-items">About</div>
                     <div className="navbar-items">Projects</div>
                     <div className="navbar-items"><Link to={{ pathname: "https://www.linkedin.com/in/fabianvillasenor/" }} target="_blank" >LinkedIn</Link></div>
