49f54e81d63d54ba9093e5a028b25c08d7ca9de7
Fabian Villasenor <exionite@gmail.com>
2022-03-31 08:59:37 -0700

    fixing

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -4,9 +4,12 @@ import './Projects.css'
 export default function Project() {
   return (
     <div className='project-container'>
-        <div className="projects"><iframe scrolling='no' src="https://devinfonseca.com/"></iframe></div>
+      <div className="projects">test</div>
+      <div className="projects">test</div>
+      <div className="projects">test</div>
+        {/* <div className="projects"><iframe scrolling='no' src="https://devinfonseca.com/"></iframe></div>
         <div className="projects"><iframe src="https://endingnever.github.io/AngularVehicleShowcase/"></iframe></div>
-        <div className="projects">Project</div>
+        <div className="projects">Project</div> */}
     </div>
   )
 }
