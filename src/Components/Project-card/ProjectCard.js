5443ecab1e98b2d16e633db243e2ff1c64ab653b
Fabian Villasenor <exionite@gmail.com>
2022-04-07 11:25:42 -0700

    mapping

--- src/Components/Project-card/ProjectCard.js
+++ src/Components/Project-card/ProjectCard.js
@@ -6,24 +6,26 @@ export default function ProjectCard(props) {
     const projectData = props.projectData;
     console.log(projectData[0].image)
     return (
-        <div className='project'>
-            {projectData.map((project)=> (
-                <>
-            <div className="project-header">
-                <h2 key={project.id}>{project.title}</h2>
-            </div>
-            <div className="project-card-container">
-                <div className="project-card-image">
-                    <img className={`img-project-${project.id}`} src={process.env.PUBLIC_URL + project.image} alt="" />
+        <>
+        {
+            projectData.map((project) => (
+                <div className='project'>
+                    <div className="project-header">
+                        <h2 key={project.id}>{project.title}</h2>
+                    </div>
+                    <div className="project-card-container">
+                        <div className="project-card-image">
+                            <img className={`img-project-${project.id}`} src={process.env.PUBLIC_URL + project.image} alt="" />
+                        </div>
+                        <div className={`project-card-text project-card-text-${project.id}`}>
+                            <p>{project.information}</p>
+                            <a href={project.link} target="_blank">{project.linkText}</a>
+                            {/* <p>Website built for Data Analyst, displaying their own projects and resume. Built in Angular, the website is a single page application using conditional rendering in order to display the current route. Check it out at <a href="https://devinfonseca.com/" target={'_blank'} >DevinFonseca.com</a>  </p> */}
+                        </div>
+                    </div>
                 </div>
-                <div className="project-card-text">
-                    <p>{project.information}</p>
-                    <a href={project.link} target="_blank">{project.linkText}</a>
-                    {/* <p>Website built for Data Analyst, displaying their own projects and resume. Built in Angular, the website is a single page application using conditional rendering in order to display the current route. Check it out at <a href="https://devinfonseca.com/" target={'_blank'} >DevinFonseca.com</a>  </p> */}
-                </div>
-            </div>
-                </>
-            ))}
-        </div>
+            ))
+        }
+    </>
     )
 }
--- src/Components/Projects/ProjectData.js
+++ src/Components/Projects/ProjectData.js
@@ -3,22 +3,26 @@
       id:1,
       title: "Website",
       image: "/images/Screenshot-dev.png",
+      information:`Website built for Data Analyst, displaying their own projects and resume. Built in Angular, the website is a single page application using conditional rendering in order to display the current route.`,
       link: "https://devinfonseca.com",
       linkText: "Click here to view the website.",
-      information:`Website built for Data Analyst, displaying their own projects and resume. Built in Angular, the website is a single page application using conditional rendering in order to display the current route.`,
       
     },
     {
       id:2,
       title: "Sign-Up Form",
       image:"/images/sign-up-form.png",
-      information: "TEst html jpg"
+      information: "Form built using React. Has email authentication and an error message if any fields are empty. ",
+      link: "https://endingnever.github.io/FrontEndForm/",
+      linkText: ""
     },
     {
       id:3,
-      // title: "project",
+      title: "project",
       image:"/images/html.png",
-      information: "TEst html jpg"
+      information: "TEst html jpg",
+      link: "",
+      linkText: ""
     }
   ]
 
